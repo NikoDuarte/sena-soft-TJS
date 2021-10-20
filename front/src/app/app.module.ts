84c1095aa921451c526946cb1108d068369e1e46
Miller Nivia <millernivia1301@gmail.com>
2021-10-20 12:44:23 -0500

    Actualización del componente Home con nuevos estilos. Creación de rutas para los componentes Home

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Rutas
+import { app_routing } from './app.routes';
+
+// Componentes
+import { HomeComponent } from './components/home/home.component';
+import { UnirseSalaComponent } from './components/unirse-sala/unirse-sala.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UnirseSalaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
