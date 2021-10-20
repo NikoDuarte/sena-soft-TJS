3367c09bccbd34a9141dba0235c23c034224cde4
Kevin Frile <kafraile@misena.edu.co>
2021-10-20 12:34:48 -0500

    comienzo de creacion del tablero del juego

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableroComponent } from './components/tablero/tablero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
