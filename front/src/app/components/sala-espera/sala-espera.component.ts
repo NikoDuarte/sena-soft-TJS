f108baee2db32268f0d6b03699392239db7005d2
KevinFrile <kafraile@misena.edu.co>
2021-10-21 07:31:05 -0500

    Implementacion de la sala de espera

---
+++ front/src/app/components/sala-espera/sala-espera.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sala-espera',
+  templateUrl: './sala-espera.component.html',
+  styleUrls: ['./sala-espera.component.css']
+})
+export class SalaEsperaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- front/src/app/pages/pages.routes.ts
+++ front/src/app/pages/pages.routes.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from '../components/home/home.component';
 import { UnirseSalaComponent } from '../components/unirse-sala/unirse-sala.component';
 import { TableroComponent } from '../components/tablero/tablero.component';
+import { SalaEsperaComponent } from '../components/sala-espera/sala-espera.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'unirse', component: UnirseSalaComponent },
-    { path: 'tablero', component:  TableroComponent}
+    { path: 'tablero', component:  TableroComponent},
+    { path: 'espera', component:  SalaEsperaComponent}
     
 
 ];
